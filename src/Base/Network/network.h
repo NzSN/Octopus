cfac2bd8b8374cd9533d4446e530ae5c550a972a
NzSN <aydenlin@foxmail.com>
2023-06-01 00:50:01 +0800

    Add base_unittests

---
+++ src/Base/Network/network.h
@@ -0,0 +1,11 @@
+#include "folly/folly/Chrono.h"
+
+
+#ifndef NETWORK_H
+#define NETWORK_H
+
+namespace Octopus {
+
+}
+
+#endif /* NETWORK_H */
--- src/Base/Network/network_unittests.cc
+++ src/Base/Network/network_unittests.cc
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include "folly/Chrono.h"
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
