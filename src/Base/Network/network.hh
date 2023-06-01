fcff70a9e1ebee699c33ca5244d09cc46a2dab13
NzSN <aydenlin@foxmail.com>
2023-06-02 01:26:11 +0800

    Add Undefined .

--- src/Base/Network/network.hh
+++ src/Base/Network/network.hh
@@ -1,4 +1,5 @@
 #include <concepts>
+#include <optional>
 #include <variant>
 #include "Base/utility.hh"
 
@@ -11,9 +12,33 @@ namespace Network {
 enum NetworkStat {};
 
 template<typename T>
-concept NetworkImpl = requires(T a) {
-    { a.asCoordinatorImpl() } -> std::convertible_to<NetworkStat>;
-    { a.asTerminalImpl() } -> std::convertible_to<NetworkStat>;
+concept NetworkTerminalImpl = requires(T t) {
+    { t.send() } -> std::same_as<size_t>;
+    { t.recv() } -> std::same_as<size_t>; };
+
+template<NetworkTerminalImpl T>
+class NetworkTerminal_ {
+public:
+protected:
+    NetworkTerminal_() = default;
+
+    #define NetworkInterfaces(V) \
+        V(size_t, send, T) \
+        V(size_t, recv, T)
+    NetworkInterfaces(CRTP_METHOD_DEFINE)
+    #undef NetworkInterfaces
+
+private:
+    UNCOPYABLE(NetworkTerminal_);
+};
+
+using NetworkTerminal = std::variant<Undefined>;
+
+
+template<typename T>
+concept NetworkImpl = requires(T t) {
+    { t.asCoordinatorImpl() } -> std::same_as<std::optional<NetworkTerminal>>;
+    { t.asTerminalImpl() } -> std::same_as<std::optional<NetworkTerminal>>;
 };
 
 /* An abstraction that represent networks for specific purposes,
@@ -25,8 +50,8 @@ template<NetworkImpl T>
 class Network {
 public:
     #define NetworkInterfaces(V) \
-        V(NetworkStat, asCoordinator, T) \
-        V(NetworkStat, asTerminal, T) \
+        V(std::optional<NetworkTerminal>, asCoordinator, T) \
+        V(std::optional<NetworkTerminal>, asTerminal, T)
     NetworkInterfaces(CRTP_METHOD_DEFINE)
     #undef NetworkInterfaces
 protected:
@@ -34,6 +59,7 @@ protected:
 private:
     UNCOPYABLE(Network);
 };
+
 
 } // Network
 } // Octopus
